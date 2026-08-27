4b89217f5de41c7ce3c022b779b4190669c1495d
Surender S. <psm.surender@gmail.com>
2026-08-27 10:02:18 -0400

    Add: binary-tree-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-1.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+        stack = deque()
+
+        curr = root
+
+        res = []
+
+        while stack or curr:
+
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            
+            curr = stack.pop()
+
+            res.append(curr.val)
+
+            curr = curr.right
+
+        return res
