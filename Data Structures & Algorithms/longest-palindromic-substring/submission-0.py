753f7fa7940859b64eba0c1ca3de97ae74a4e96c
Surender S. <psm.surender@gmail.com>
2026-09-03 15:10:57 -0400

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # This is 2 pointer problem not dp
+
+        # 2 cases: odd and even palindromes
+
+        res = ""
+        resLen = 0
+
+        for i in range(len(s)):
+
+            # odd
+            l,r = i,i
+            while l >= 0 and r < len(s) and s[r] == s[l]:
+                if resLen < r-l+1:
+                    res = s[l:r+1]
+                    resLen = r-l+1
+                l-=1
+                r+=1
+            
+            # even
+            l,r = i,i+1
+            while l >= 0 and r<len(s) and s[r] == s[l]:
+                if resLen < r-l+1:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l-=1
+                r+=1
+
+        return res
